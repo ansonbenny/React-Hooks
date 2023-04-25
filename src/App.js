ce7101c5f4bee857fc611ed99644050b4b827416
anson <ansonbenny16@gmail.com>
2023-04-25 11:36:23 +0530

    custom hook

--- src/App.js
+++ src/App.js
@@ -5,102 +5,138 @@ import ImperativeHandle from "./useImperativeHandle/ImperativeHandle";
 import Transition from "./useTransition/Transition";
 import Memo from "./useMemo/Memo";
 import Callback from "./useCallback/parent";
+import CustomParent from "./custom/parent";
 
 const stateControl = (state, action) => {
   switch (action.type) {
     case "home":
-      return { for: '', home: true }
+      return { for: "", home: true };
     case "useReducer":
-      return { for: 'useReducer', home: false }
+      return { for: "useReducer", home: false };
     case "useLayoutEffect":
-      return { for: 'useLayoutEffect', home: false }
+      return { for: "useLayoutEffect", home: false };
     case "useImperativeHandle":
-      return { for: 'useImperativeHandle', home: false }
+      return { for: "useImperativeHandle", home: false };
     case "useTransition":
-      return { for: 'useTransition', home: false }
+      return { for: "useTransition", home: false };
     case "useMemo":
-      return { for: 'useMemo', home: false }
+      return { for: "useMemo", home: false };
     case "useCallback":
-      return { for: 'useCallback', home: false }
+      return { for: "useCallback", home: false };
+    case "custom":
+      return { for: "custom", home: false };
     default:
-      return state
+      return state;
   }
-}
+};
 
 function App() {
   const [state, dispatch] = useReducer(stateControl, {
-    for: '',
-    home: true
-  })
+    for: "",
+    home: true,
+  });
   return (
     <div className="App">
-      {
-        state.home ? (
-          <>
-            <br />
-            <button onClick={() => {
-              dispatch({ type: 'useReducer' })
-            }} >useReducer</button>
+      {state.home ? (
+        <>
+          <br />
+          <button
+            onClick={() => {
+              dispatch({ type: "useReducer" });
+            }}
+          >
+            useReducer
+          </button>
 
-            <br /><br />
+          <br />
+          <br />
 
-            <button onClick={() => {
-              dispatch({ type: 'useLayoutEffect' })
-            }} >useLayoutEffect </button>
+          <button
+            onClick={() => {
+              dispatch({ type: "useLayoutEffect" });
+            }}
+          >
+            useLayoutEffect
+          </button>
 
-            <br /><br />
+          <br />
+          <br />
 
-            <button onClick={() => {
-              dispatch({ type: 'useImperativeHandle' })
-            }} >useImperativeHandle </button>
+          <button
+            onClick={() => {
+              dispatch({ type: "useImperativeHandle" });
+            }}
+          >
+            useImperativeHandle
+          </button>
 
-            <br /><br />
+          <br />
+          <br />
 
-            <button onClick={() => {
-              dispatch({ type: 'useTransition' })
-            }} >useTransition </button>
+          <button
+            onClick={() => {
+              dispatch({ type: "useTransition" });
+            }}
+          >
+            useTransition
+          </button>
 
-            <br /><br />
+          <br />
+          <br />
 
-            <button onClick={() => {
-              dispatch({ type: 'useMemo' })
-            }} >useMemo </button>
+          <button
+            onClick={() => {
+              dispatch({ type: "useMemo" });
+            }}
+          >
+            useMemo
+          </button>
 
-            <br /><br />
+          <br />
+          <br />
 
-            <button onClick={() => {
-              dispatch({ type: 'useCallback' })
-            }} >useCallback </button>
-          </>
-        ) : <button onClick={() => {
-          dispatch({ type: 'home' })
-        }} >Home</button>
-      }
+          <button
+            onClick={() => {
+              dispatch({ type: "useCallback" });
+            }}
+          >
+            useCallback
+          </button>
 
+          <br />
+          <br />
 
-      {
-        state.for === 'useReducer' && <Reducer />
-      }
+          <button
+            onClick={() => {
+              dispatch({ type: "custom" });
+            }}
+          >
+            custom
+          </button>
+        </>
+      ) : (
+        <button
+          onClick={() => {
+            dispatch({ type: "home" });
+          }}
+        >
+          Home
+        </button>
+      )}
 
-      {
-        state.for === 'useLayoutEffect' && <LayoutEffect />
-      }
+      {state.for === "useReducer" && <Reducer />}
 
-      {
-        state.for === 'useImperativeHandle' && <ImperativeHandle />
-      }
+      {state.for === "useLayoutEffect" && <LayoutEffect />}
 
-      {
-        state.for === 'useTransition' && <Transition />
-      }
+      {state.for === "useImperativeHandle" && <ImperativeHandle />}
 
-      {
-        state.for === 'useMemo' && <Memo />
-      }
+      {state.for === "useTransition" && <Transition />}
 
-      {
-        state.for === 'useCallback' && <Callback />
-      }
+      {state.for === "useMemo" && <Memo />}
+
+      {state.for === "useCallback" && <Callback />}
+
+      {state.for === "custom" && <CustomParent />}
     </div>
   );
 }
